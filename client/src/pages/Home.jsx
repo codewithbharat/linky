c76a840ec78c29382602b28bfecdf9e54cfb4fa3
Bharat Ranjan <bhaarat.ranjan@gmail.com>
2023-12-18 13:47:25 +0530

    responsive

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -43,13 +43,13 @@ const Home = () => {
     return (
         <Layout>
             <div className="flex justify-center items-center">
-                <div className="flex flex-col text-center m-10">
-                    <h1 className='text-5xl font-bold leading-loose drop-shadow-sm bg-gradient-to-r from-sky-400 to-red-400 bg-clip-text text-transparent'>Shorten Your Loooong Links!!</h1>
-                    <p className='text-semibold tracking-wide'>Linky is an URL shortening service that streamlines your online experience.</p>
-                    <div className="flex flex-row justify-center p-4">
-                        <form onSubmit={handelSubmit}>
-                            <input type="text" className="rounded-l-lg p-2 border-t m-4 w-96 mr-0 border-b border-l text-md text-slate-800 border-yellow-500 bg-white " placeholder="Your Ugly Url...." />
-                            <input type="submit" className="px-8 py-2 rounded-r-lg bg-gradient-to-r from-cyan-700 to-red-400  font-bold uppercase z-10 border-yellow-500 border-t border-b border-r shadow-lg cursor-pointer" />
+                <div className="flex flex-col items-center text-center m-10">
+                    <h1 className='text-2xl md:text-4xl font-bold leading-loose drop-shadow-sm bg-gradient-to-r from-sky-400 to-red-400 bg-clip-text text-transparent'>Shorten Your Loooong Links!!</h1>
+                    <p className=' w-2/3 md:text-xl md:pt-4 text-semibold tracking-wide'>Linky is an URL shortening service that streamlines your online experience.</p>
+                    <div className="flex items-center justify-center p-4">
+                        <form className='flex flex-col md:flex-row justify-center items-center w-2/3' onSubmit={handelSubmit}>
+                            <input type="text" className="rounded-lg md:rounded-none md:rounded-l-lg p-2 m-4 md:w-96 mr-0 text-md text-slate-800 border-yellow-500 bg-white " placeholder="Your Ugly Url...." />
+                            <input type="submit" className="px-8 py-2 rounded-lg md:rounded-none md:rounded-r-lg bg-gradient-to-r from-cyan-700 to-red-400  font-bold uppercase z-10 border-yellow-500 border-t border-b border-r shadow-lg cursor-pointer" />
                         </form>
                     </div>
                     {showModal && <Modal url={`${import.meta.env.VITE_BASE_URL}/${shortUrl}`} onClose={handleCloseModal} />} {/* Render the Modal component */}
